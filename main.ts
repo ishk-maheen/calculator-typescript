93b9bc7abd83600998b9e34441bee9ed588a439a
MAHEEN SHEIKH <160344462+ishk-maheen@users.noreply.github.com>
2024-05-31 00:48:57 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const answers = await inquirer.prompt([
+    {
+        name: "firstNumber",
+        type: "number",
+        message: "Enter the first number"
+    },
+    {
+        name: "secondNumber",
+        type: "number",
+        message: "Enter the second number"
+    },
+    {
+        name: "operators",
+        type: "list",
+        message: "Enter the operators",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division", "Modulus", "Exponentiation"]
+    }
+]);
+
+if(answers.operators === "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber);
+} else if(answers.operators === "Subtraction"){
+    console.log(answers.firstNumber - answers.secondNumber);
+} else if(answers.operators === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber);
+} else if(answers.operators === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber);
+} else if(answers.operators === "Modulus"){
+    console.log(answers.firstNumber % answers.secondNumber);
+} else if(answers.operators === "Exponentiation"){
+    console.log(answers.firstNumber ** answers.secondNumber);
+} else {
+    console.log(chalk.red.bold("Please select operators"));
+}
+
+
+
+
+
+
+
+
+
+
